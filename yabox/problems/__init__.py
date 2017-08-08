9724dd18e6e8924b03649ba38b33d4d80f6e559d
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2017-08-08 12:54:57 +0200

    Import all functions from problems/.base

--- yabox/problems/__init__.py
+++ yabox/problems/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
-from .base import (BaseProblem, Ackley, Rastrigin, Rosenbrock, CrossInTray,
-                   EggHolder, HolderTable, Easom, Slowdown)
+from .base import *
